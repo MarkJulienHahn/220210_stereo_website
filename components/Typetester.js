6891e7c93a33e6de69de23f8e6c314d6a0810f1d
Mark Julien Hahn <mail@markjulienhahn.de>
2022-02-11 18:37:04 +0100

    ...

--- components/Typetester.js
+++ components/Typetester.js
@@ -101,7 +101,7 @@ function Typetester ( props ) {
             </div>
         </div>
 
-        <div className={styles.inputWrapper}>
+        <div className="inputWrapper">
           <div contentEditable="true" className={styles.input} style={testerStyle} type="text" name="lname">
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
           </div>
