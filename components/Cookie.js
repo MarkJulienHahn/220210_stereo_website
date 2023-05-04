55a57c53d3fca2c1cd10ac4643b05d6e75b0bbb3
Mark Julien Hahn <mail@markjulienhahn.de>
2023-05-04 17:47:01 +0200

    ...

--- components/Cookie.js
+++ components/Cookie.js
@@ -2,12 +2,16 @@ import Link from "next/link";
 import Button from "../components/Button";
 
 const Cookie = ({ onAccept, onDecline }) => {
-
   return (
     <>
-      By continuing to browse this site, you agree to the use of cookies to
-      identify your session and to remember your login after you close the
-      browser (authentication cookies).
+      We use cookies to provide you with a better browsing experience and to
+      analyze site traffic. Protecting your privacy and personal data is
+      important to us, which is why we want to inform you about our use of
+      cookies in compliance with the General Data Protection Regulation (GDPR).
+      <br />
+      <br />
+      If you do not wish to allow any cookies, please click »Decline«.
+      Please note that this may limit the functionality of the website.
       <div className="cookieButtonWrapper">
         <Button lable={"Accept"} subclass={"primary"} onClick={onAccept} />
         <Button lable={"Decline"} subclass={"secondary"} onClick={onDecline} />
