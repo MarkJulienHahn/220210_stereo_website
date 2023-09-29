b1dc3f5dd382ba9b6225d3667a2c852d0c76821b
Mark Julien Hahn <mail@markjulienhahn.de>
2023-09-29 19:15:10 +0200

    Cookie Note Change

--- components/Cookie.js
+++ components/Cookie.js
@@ -4,14 +4,8 @@ import Button from "../components/Button";
 const Cookie = ({ onAccept, onDecline }) => {
   return (
     <>
-      We use cookies to provide you with a better browsing experience and to
-      analyze site traffic. Protecting your privacy and personal data is
-      important to us, which is why we want to inform you about our use of
-      cookies in compliance with the General Data Protection Regulation (GDPR).
-      <br />
-      <br />
-      If you do not wish to allow any cookies, please click »Decline«.
-      Please note that this may limit the functionality of the website.
+      If you do not wish to allow any cookies, please click »Decline«. Please
+      note that this may limit the functionality of the website.
       <div className="cookieButtonWrapper">
         <Button lable={"Accept"} subclass={"primary"} onClick={onAccept} />
         <Button lable={"Decline"} subclass={"quaternary"} onClick={onAccept} />
