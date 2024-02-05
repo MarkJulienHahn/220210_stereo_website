23f32cf18be165a09f2ac4ecc1a50ac1dd03a12f
Mark Julien Hahn <mail@markjulienhahn.de>
2024-02-05 11:40:09 +0100

    Preload Fonts

--- components/Layout.js
+++ components/Layout.js
@@ -17,6 +17,27 @@ export default function RootLayout({ children }) {
         />
         <link rel="icon" href="/favicon.ico" sizes="any"></link>
         <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
+        <link
+          rel="preload"
+          href="/fonts/ProtestGrotesk.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/ProtestGroteskMono.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/GialloRoman.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
       </Head>
       <body>
         {children}
